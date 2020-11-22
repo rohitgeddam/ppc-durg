2fcdb333d1ef3dc71ebdebd530f85326de77d7c9
rohitgeddam <rohitgeddam2018@gmail.com>
2020-11-22 19:44:55 +0530

    Feat: Implement Search in members list

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from memberships.models import Member
+from django.db.models import Q
+from django.db.models import Value as V
+from django.db.models.functions import Concat
 
 # Create your views here.
 class MemberList(ListView):
@@ -8,3 +11,18 @@ class MemberList(ListView):
     template_name = "portal/members/list.html"
     context_object_name = "members_list"
     paginate_by = 20
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            object_list = self.model.objects.annotate(
+                full_name=Concat("first_name", V(" "), "last_name")
+            ).filter(
+                Q(first_name__icontains=query)
+                | Q(last_name__icontains=query)
+                | Q(membership_id__icontains=query)
+                | Q(full_name__icontains=query)
+            )
+        else:
+            object_list = self.model.objects.all()
+        return object_list
